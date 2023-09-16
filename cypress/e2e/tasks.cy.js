781f5592282bccdaf9a946912c1760ccaa61653b
Phone Naing <phone.naing.dev@gmail.com>
2023-09-16 16:08:35 +0100

    test(new task): it adds multiple tasks

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -46,5 +46,44 @@ describe('tasks management', () => {
 
         // check for error message
         cy.get('.modal').contains('Please provide values');
-    })
+    });
+
+    it('should filter tasks', () => {
+        cy.visit('http://localhost:5173/');
+        cy.contains('Add Task').click();
+        // simulate typing
+        cy.get('#title').type('New Task');
+        cy.get('#summary').type('Some description');
+
+        // pick the 'urgent' category
+        cy.get('#category').select('urgent');
+        cy.get('.modal').contains('Add Task').click();
+        cy.get('.task').should('have.length', 1);
+
+        // pick the filter option of 'moderate'
+        cy.get('#filter').select('moderate');
+        // expect this to be empty
+        cy.get('.task').should('have.length',0);
+    });
+
+    it('should add multiple tasks', () => {
+        cy.visit('http://localhost:5173/');
+        cy.contains('Add Task').click();
+        // simulate typing
+        cy.get('#title').type('Task 1');
+        cy.get('#summary').type('First task');
+        cy.get('.modal').contains('Add Task').click();
+        // add second task
+        cy.contains('Add Task').click();
+        cy.get('#title').type('Task 2');
+        cy.get('#summary').type('Second task');
+        cy.get('.modal').contains('Add Task').click();
+
+        // check that baoth tasks are added
+        cy.get('.task').should('have.length', 2);
+        // check they are in correct order
+        cy.get('.task').eq(0).contains('First task');
+        cy.get('.task').eq(1).contains('Second task');
+
+    });
 })
