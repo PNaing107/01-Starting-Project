4462d6bde1d9e7162227a04a980d08f7948c1b93
Phone Naing <phone.naing.dev@gmail.com>
2023-09-16 10:08:48 +0100

    test(new task): it should create a new task

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -19,5 +19,23 @@ describe('tasks management', () => {
         // modal should have closed
         cy.get('.backdrop').should('not.exsist');
         cy.get('.modal').should('not.exist'); 
+    });
+
+    it('should create a new task', () => {
+        cy.visit('http://localhost:5173/');
+        cy.contains('Add Task').click();
+        // simulate typing
+        cy.get('#title').type('New Task');
+        cy.get('#summary').type('Some description');
+
+        // check it was created
+        cy.get('.modal').contains('Add Task').click();
+        cy.get('.task').should('have.length', 1);
+        cy.get('.task h2').contains('New Task');
+        cy.get('.task p').contains('Some description');
+
+        // check the modal is closed after adding a new task
+        cy.get('.backdrop').should('not.exsist');
+        cy.get('.modal').should('not.exist');
     })
 })
